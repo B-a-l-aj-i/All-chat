90339e88906bd801555e8aaf62b9b8b2fa2f9aa2
B-a-l-aj-i <balajivijay679@gmail.com>
2026-07-21 10:44:56 +0530

    refactor(db): rename migration and update room schema to include owner_id
    Remove the previous migration for typical songbird and rename it to reflect the addition of the 'owner_id' field in the room schema. This change establishes a reference to the user table, enhancing the room's data structure.

--- packages/db/src/schema/room.ts
+++ packages/db/src/schema/room.ts
@@ -1,11 +1,13 @@
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { createId } from "@paralleldrive/cuid2";
+import { user } from "./user";
 
 export const room = sqliteTable("room", {
   id: text("id").primaryKey().$defaultFn(() => createId()),
   room_name: text("room_name").notNull().unique(),
   description: text("description"),
   password: text("password").notNull(),
+  owner_id: text("owner_id").references(() => user.id),
 });
 
 export type Room = typeof room.$inferSelect;
